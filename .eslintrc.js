0d5444303a2ab5ba456eeb1c31e4dda2edba9c03
shinker1002 <shinker1002@naver.com>
2023-08-28 16:58:20 +0900

    Fix: eslint 에러 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,24 +6,25 @@ module.exports = {
   ],
   extends: [
     'airbnb',
-    // 'plugin:react/recommended', // eslint-plugin-react 설치한 경우
-    // 'plugin:jsx-a11y/recommended', // eslint-plugin-jsx-a11y 설치한 경우
-    // 'plugin:import/errors', // eslint-plugin-import 설치한 경우
-    // 'plugin:import/warnings', // eslint-plugin-import 설치한 경우
+    'plugin:react/recommended', // eslint-plugin-react 설치한 경우
+    'plugin:jsx-a11y/recommended', // eslint-plugin-jsx-a11y 설치한 경우
+    'plugin:import/errors', // eslint-plugin-import 설치한 경우
+    'plugin:import/warnings', // eslint-plugin-import 설치한 경우
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   rules: {
     'prettier/prettier': 0,
+    "react/jsx-filename-extension":["warn",{"extensions":[".tsx"]}],
   },
   settings: {
     'import/resolver': {
       node: {
         extensions: [
           '.js',
-          //   '.jsx',
+          '.jsx',
           '.ts',
-          //   '.tsx',
+          '.tsx',
         ],
       },
     },
